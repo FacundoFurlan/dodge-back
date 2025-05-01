dff7b5acb3b37e262194c1c712b2d9da1dcdd7c3
NeroWork <facundofurlan7@gmail.com>
2025-05-01 00:08:09 -0300

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.get("/", (req, res) => {
 app.get("/api/scores", async (req, res) => {
     try {
       const topScores = await Score.find().sort({ score: -1 }).limit(3);
-      res.status(200).json(topScores);
+      res.send("se encontro algo").status(200).json(topScores);
     } catch (err) {
       res.status(500).send("Error al obtener puntajes.");
     }
